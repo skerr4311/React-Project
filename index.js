59495e76a9d19df03562537392ae9b4f16245359
SkerrHome <sker078@aucklanduni.ac.nz>
2019-11-25 08:35:41 +1300

    making components

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //import libraries
 import React from "react";
-import ReactNative from "react-native";
+import {Text, AppRegistry} from "react-native";
+
+import Header from './src/components/Header';
 
 //create component
 const App = () => {
-    return (
-        <Text>Hello World</Text>
-    );
+    return <Header />;
     //this fuction must return an object that 
     //describes what it should look like on a device
 }
 
 //render component to screen
+AppRegistry.registerComponent('cars', () => App); 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+//import libraries
+import React from 'react';
+import {Text} from 'react-native';
+
+//create component
+const Header = () => {
+    return (
+        <Text>Cars</Text>
+    );
+}
+
+//make component available to other components
+export default Header;
