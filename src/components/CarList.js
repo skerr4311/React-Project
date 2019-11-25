07355a5d962dc4e9c24adb0220d1db87fe73412d
SkerrHome <sker078@aucklanduni.ac.nz>
2019-11-26 09:14:06 +1300

    installed axiom

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,14 @@
 import React, {Component} from "react";
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class CarList extends Component {
+    componentDidMount() {
+        axios.get('https://givecars.herokuapp.com')
+        .then((response) => {
+            console.log(response)
+        })
+    }
     render() {
         return (
             <View>
